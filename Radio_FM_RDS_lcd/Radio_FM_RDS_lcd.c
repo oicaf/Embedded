5dccd453a5312f04a9f2d71192a46913de66e352
oicaf <162804996+oicaf@users.noreply.github.com>
2024-03-14 11:52:25 +0100

    Add files via upload
    - declaration of own pointer variables assigned to memory addresses that correspond to ATmega328 registers
    - variables declaration moved from .c file to header file
    - some comments added

--- Radio_FM_RDS_lcd/Radio_FM_RDS_lcd.c
+++ Radio_FM_RDS_lcd/Radio_FM_RDS_lcd.c
@@ -1,11 +1,7 @@
 #include <string.h>
-#include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "Radio_FM_RDS_lcd.h"
-
-char RDS_PS[9] = "        "; // array for RDS Program Service text (max 8 chars + NULL char '\0')
-unsigned short int registers[16]; // 16 x 16-bit device registers
 
 
 /*** BIT OPERATIONS ***/
@@ -22,21 +18,21 @@ void cbit(volatile unsigned char *reg, unsigned char bit) // clear bit in regist
 
 void _sbit(unsigned char pin) // set bit on port C
 {
-  /* if _sbit is replaced in the code below in a few places by sbit(&PORTC, pin) then functionality behaves strange, don't know the reason */
-  PORTC |= (1 << pin);
+  /* if at least one more _sbit is replaced in the code below by sbit(&PORTC, pin) then functionality starts behaving strange, don't know the reason */
+  *_PORTC |= (1 << pin);
 }
 
 void dir_sda(unsigned char dir) // set SDA direction
 {
   if (dir == 0)
-    DDRC &= ~(1 << SDA); // SDA as input
+    cbit(_DDRC, SDA); // SDA as input
   else if (dir == 1)
-    DDRC |= (1 << SDA); // SDA as output
+    sbit(_DDRC, SDA); // SDA as output
 }
 
 unsigned char read_sda() // read bit on SDA
 {
-  unsigned char data = PINC; // read byte
+  unsigned char data = *_PINC; // read byte
   data &= (1 << SDA); // mask on SDA
   return (data != 0) ? 1 : 0;
 }
@@ -45,31 +41,31 @@ unsigned char read_sda() // read bit on SDA
 
 void start() // start bit
 {
-  _sbit(SDA);
-  _sbit(SCL);
-  cbit(&PORTC, SDA);
-  cbit(&PORTC, SCL);
+  sbit(_PORTC, SDA);
+  sbit(_PORTC, SCL);
+  cbit(_PORTC, SDA);
+  cbit(_PORTC, SCL);
 }
 
 void stp() // stop bit
 {
-  cbit(&PORTC, SDA);
-  _sbit(SCL);
-  _sbit(SDA);
+  cbit(_PORTC, SDA);
+  sbit(_PORTC, SCL);
+  sbit(_PORTC, SDA);
 }
 
 void ack() // acknowledgment bit
 {
-  cbit(&PORTC, SDA);
-  _sbit(SCL);
-  cbit(&PORTC, SCL);
+  cbit(_PORTC, SDA);
+  sbit(_PORTC, SCL);
+  cbit(_PORTC, SCL);
 }
 
 void nack() // no acknowledgment bit
 {
-  _sbit(SDA);
-  _sbit(SCL);
-  cbit(&PORTC, SCL);
+  sbit(_PORTC, SDA);
+  sbit(_PORTC, SCL);
+  cbit(_PORTC, SCL);
 }
 
 unsigned char read_byte() // read 8-bits
@@ -84,7 +80,7 @@ unsigned char read_byte() // read 8-bits
       data |= 1;
     if (i < 7)
       data <<= 1;
-    cbit(&PORTC, SCL);
+    cbit(_PORTC, SCL);
   }
   dir_sda(_OUTPUT);
   
@@ -99,20 +95,20 @@ void write_byte(unsigned char data) // write 8-bits
   {
     if ((data & 0x80) == 0)
     {
-      cbit(&PORTC, SDA);
+      cbit(_PORTC, SDA);
     }
     else
     {
       _sbit(SDA);
     }
     _sbit(SCL);
-    cbit(&PORTC, SCL);
+    cbit(_PORTC, SCL);
     data <<= 1;
   }
   
   dir_sda(_INPUT);
   _sbit(SCL);
-  cbit(&PORTC, SCL);
+  cbit(_PORTC, SCL);
   dir_sda(_OUTPUT);
 }
 
@@ -120,41 +116,41 @@ void write_byte(unsigned char data) // write 8-bits
 
 void init_interrupt() // interrupt configuration
 {
-  cbit(&DDRD, INT); // INT as input
-  sbit(&PORTD, INT); // pull-up on INT
-  sbit(&EICRA, ISC11); // falling edge on INT1 generates an interrupt request
-  sbit(&EIMSK, INT1); // external interrupt request 1 enable
-  sbit(&SREG, 7); // enable global interrupts
+  cbit(_DDRD, INT); // INT as input
+  sbit(_PORTD, INT); // pull-up on INT
+  sbit(_EICRA, _ISC11); // falling edge on INT1 generates an interrupt request
+  sbit(_EIMSK, _INT1); // external interrupt request 1 enable
+  sbit(_SREG, 7); // enable global interrupts
 }
 
 void clr_eeprom() // eeprom clean out from 0x08 up to 0x00 addresses
 {
   for (char i = 8; i >= 0; i--)
   {
-    while (EECR & (1 << EEPE)); // wait for completion of previous write
-    EEARH = 0; // eeprom address register (high byte)
-    EEARL = i; // eeprom address register (low byte)
-    EEDR = 0; // eeprom data register
-    sbit(&EECR, EEMPE);
-    sbit(&EECR, EEPE); // start eeprom writing
+    while (*_EECR & (1 << _EEPE)); // wait for completion of previous write
+    *_EEARH = 0; // eeprom address register (high byte)
+    *_EEARL = i; // eeprom address register (low byte)
+    *_EEDR = 0; // eeprom data register
+    sbit(_EECR, _EEMPE);
+    sbit(_EECR, _EEPE); // start eeprom writing
   }
 }
 
 void read_eeprom(unsigned char favourite) // read data from eeprom, address at current favourite
 {
-  while (EECR & (1 << EEPE)); // wait for completion of previous write
-  EEARH = 0; // eeprom address register (high byte)
-  EEARL = favourite - 1; // eeprom address register (low byte)
-  sbit(&EECR, EERE); // start eeprom reading
-  registers[0x03] |= EEDR; // mask on new channel (eeprom data register)
+  while (*_EECR & (1 << _EEPE)); // wait for completion of previous write
+  *_EEARH = 0; // eeprom address register (high byte)
+  *_EEARL = favourite - 1; // eeprom address register (low byte)
+  sbit(_EECR, _EERE); // start eeprom reading
+  registers[0x03] |= *_EEDR; // mask on new channel (eeprom data register)
 }
 
 void write_eeprom() // write data into eeprom, address at current favourite
 {
-  while (EECR & (1 << EEPE)); // wait for completion of previous write
-  EEDR = registers[0x0B] & 0x00FF; // eeprom data register, mask on channel, only 8 bits is sufficient
-  sbit(&EECR, EEMPE);
-  sbit(&EECR, EEPE); // start eeprom writing
+  while (*_EECR & (1 << _EEPE)); // wait for completion of previous write
+  *_EEDR = registers[0x0B] & 0x00FF; // eeprom data register, mask on channel, only 8 bits is sufficient
+  sbit(_EECR, _EEMPE);
+  sbit(_EECR, _EEPE); // start eeprom writing
 }
 
 /*** Si4703 ***/
@@ -202,15 +198,15 @@ void write_registers() // device registers (6) update
 
 void init_radio() // Si4703 initialization
 {
-  sbit(&DDRC, SDA); // SDA as output
-  sbit(&DDRC, SCL); // SCL as output
-  sbit(&DDRD, RST); // RST as output
+  sbit(_DDRC, SDA); // SDA as output
+  sbit(_DDRC, SCL); // SCL as output
+  sbit(_DDRD, RST); // RST as output
 
   _sbit(SCL);
-  cbit(&PORTC, SDA);
-  cbit(&PORTD, RST);
+  cbit(_PORTC, SDA);
+  cbit(_PORTD, RST);
   _delay_ms(1);
-  sbit(&PORTD, RST);
+  sbit(_PORTD, RST);
   _delay_ms(1);
  
   read_registers();
@@ -227,7 +223,7 @@ void init_radio() // Si4703 initialization
 }
 
 void set_channel(unsigned char key) // tune into selected channel / frequency
-{        
+{
   strcpy(RDS_PS, "        "); // Program Service text clean out when favourite station selected
 
   read_registers();
@@ -300,7 +296,7 @@ void set_volume(unsigned char vol)
 
 ISR(INT1_vect) // Interrupt Service Routine from INT1 (RDS Program Service text decode)
 {
-  char c_SREG = SREG; // store Status REGister
+  char c_SREG = *_SREG; // store Status REGister
   unsigned char PS_index; // RDS received char index
   unsigned char rx_char; // RDS received char
   
@@ -318,44 +314,44 @@ ISR(INT1_vect) // Interrupt Service Routine from INT1 (RDS Program Service text 
       rx_char = ' ';    
     RDS_PS[PS_index * 2 + 1] = rx_char;
   }
-  SREG = c_SREG; // restore Status REGister
+  *_SREG = c_SREG; // restore Status REGister
 }
 
 /*** 2x16 LCD DISPLAY (HD44780) ***/
 
 void strobe() // device strobe
 {
-  sbit(&PORTB, EN);
-  cbit(&PORTB, EN);
+  sbit(_PORTB, EN);
+  cbit(_PORTB, EN);
 }
 
-void write_display(unsigned char mode, unsigned char data)
+void write_display(unsigned char mode, unsigned char data) // write instruction or data to display
 {
   if (mode == 0) // instruction register
-    cbit(&PORTB, RS);
+    cbit(_PORTB, RS);
   if (mode == 1) // data register
-    sbit(&PORTB, RS);
+    sbit(_PORTB, RS);
 
-  PORTD &= 0x0F; // clear four upper bits on port D
-  PORTD |= (data & 0xF0); // mask on the upper nibble
+  *_PORTD &= 0x0F; // clear four upper bits on port D
+  *_PORTD |= (data & 0xF0); // mask on the upper nibble
   strobe();
-  PORTD &= 0x0F; // clear four upper bits on port D
-  PORTD |= ((data << 4) & 0xF0); // mask on the lower nibble
+  *_PORTD &= 0x0F; // clear four upper bits on port D
+  *_PORTD |= ((data << 4) & 0xF0); // mask on the lower nibble
   strobe();
 }
 
-void init_display() // LCD initialization
+void init_display() // display initialization
 {
-  sbit(&DDRB, RS); // RS as output
-  sbit(&DDRB, EN); // EN as output
-  DDRD |= 0xF0; // D4...D7 as output
+  sbit(_DDRB, RS); // RS as output
+  sbit(_DDRB, EN); // EN as output
+  *_DDRD |= 0xF0; // D4...D7 as output
   
-  cbit(&PORTB, EN);
-  cbit(&PORTB, RS);
+  cbit(_PORTB, EN);
+  cbit(_PORTB, RS);
   _delay_ms(150);
 
-  PORTD &= 0x0F; // clear four upper bits on port D
-  PORTD |= 0x30;
+  *_PORTD &= 0x0F; // clear four upper bits on port D
+  *_PORTD |= 0x30;
   strobe();
   _delay_ms(5);
 
@@ -374,13 +370,13 @@ void init_display() // LCD initialization
   _delay_ms(1);
 }
 
-void write_char(unsigned char data) // char display
+void write_char(unsigned char data) // display char
 {
   write_display(DATA, data);
   _delay_ms(1);
 }
 
-void write_text(char *text) // text display
+void write_text(char *text) // display text
 {
   unsigned char i = 0;
 
@@ -398,7 +394,7 @@ void display_info() // information display
   read_registers();
 
   write_text("FM:");
-  write_char(EEARL + 1 + 0x30); // favourite nr
+  write_char(*_EEARL + 1 + 0x30); // favourite nr
   
   write_text("   ");
 
--- Radio_FM_RDS_lcd/Radio_FM_RDS_lcd.h
+++ Radio_FM_RDS_lcd/Radio_FM_RDS_lcd.h
@@ -1,71 +1,98 @@
 #define _INPUT 0
 #define _OUTPUT 1
-
 #define INST 0
 #define DATA 1
 
-/*** HARDWIRED PINS ***/
-#define SDA 4 // SDA on port C pin 4 (PC4)
-#define SCL 5 // SCL on port C pin 5 (PC5)
-#define RST 2 // RST on port D pin 2 (PD2)
-#define INT 3 // INT1 on port D pin 3 (PD3)
-#define RS 0 // RS on port B pin 0 (PB0)
-#define EN 1 // EN on port B pin 1 (PB1)
+/*** ATmega328 ***/
+#define SDA 4 // I2C Serial Data on port C pin 4
+#define SCL 5 // I2C Serial Clock on port C pin 5
+#define RST 2 // Reset on port D pin 2
+#define INT 3 // INTerrupt (INT1) on port D pin 3
+#define RS 0 // LCD Register Select on port B pin 0
+#define EN 1 // LCD ENable on port B pin 1
+#define _ISC11 3 // Interrupt Sense Control 1 bit 1
+#define _INT1 1 // External Interrupt Request 1 Enable
+#define _EEPE 1 // EEPROM Write Enable
+#define _EEMPE 2 // EEPROM Master Write Enable
+#define _EERE 0 // EEPROM Read Enable
 
-/*** REGISTER 0x02 ***/
-#define SEEK 8
-#define SEEKUP 9
-
-/*** REGISTER 0x03 ***/
-#define TUNE 15
-
-/*** REGISTER 0x04 ***/
-#define GPIO2 2
-#define DE 11
-#define RDS 12
-#define RDSIEN 15
-
-/*** REGISTER 0x05 ***/
-#define SPACE 4
-
-/*** REGISTER 0x0A ***/
-#define STC 14
-#define RDSR 15
+/*** Si4703 REGISTERS ***/
+/* 0x02 */
+#define SEEK 8 // Seek
+#define SEEKUP 9 // Seek Direction
+/* 0x03 */
+#define TUNE 15 // Tune
+/* 0x04 */
+#define GPIO2 2 // General Purpose I/O 2
+#define DE 11 // De-emphasis
+#define RDS 12 // RDS Enable
+#define RDSIEN 15 // RDS Interrupt Enable
+/* 0x05 */
+#define SPACE 4 // Channel Spacing
+/* 0x0A */
+#define STC 14 // Seek/Tune Complete
+#define RDSR 15 // RDS Ready
 
 /*** VARIABLES AND FUNCTIONS DECLARATION ***/
+static volatile unsigned char *_DDRB = (volatile unsigned char *)0x24; // Port B Data Direction Register
+static volatile unsigned char *_PORTB = (volatile unsigned char *)0x25; // Port B Data Register
+static volatile unsigned char *_PINC = (volatile unsigned char *)0x26; // Port C Input Pins Address
+static volatile unsigned char *_DDRC = (volatile unsigned char *)0x27; // Port C Data Direction Register
+static volatile unsigned char *_PORTC = (volatile unsigned char *)0x28; // Port C Data Register
+static volatile unsigned char *_DDRD = (volatile unsigned char *)0x2A; // Port D Data Direction Register
+static volatile unsigned char *_PORTD = (volatile unsigned char *)0x2B; // Port D Data Register
+static volatile unsigned char *_EIMSK = (volatile unsigned char *)0x3D; // External Interrupt Mask Register
+static volatile unsigned char *_EECR = (volatile unsigned char *)0x3F; // EEPROM Control Register
+static volatile unsigned char *_EEDR = (volatile unsigned char *)0x40; // EEPROM Data Register
+static volatile unsigned char *_EEARL = (volatile unsigned char *)0x41; // EEPROM Address Register (Low)
+static volatile unsigned char *_EEARH = (volatile unsigned char *)0x42; // EEPROM Address Register (High)
+static volatile unsigned char *_SREG = (volatile unsigned char *)0x5F; // Status Register
+static volatile unsigned char *_EICRA = (volatile unsigned char *)0x69; // External Interrupt Control Register A
+
 unsigned char key; // input key on serial monitor
+unsigned short int registers[16]; // 16 x 16-bit device registers
+static char RDS_PS[9] = "        "; // array for RDS Program Service text (max 8 chars + NULL char '\0')
 
 #ifdef __cplusplus
  extern "C" {
 #endif
 
-void sbit(volatile unsigned char *reg, unsigned char bit);
-void cbit(volatile unsigned char *reg, unsigned char bit);
-void _sbit(unsigned char pin);
-void dir_sda(unsigned char dir);
-unsigned char read_sda();
-void start();
-void stp();
-void ack();
-void nack();
-unsigned char read_byte();
-void write_byte(unsigned char data);
-void init_interrupt();
-void clr_eeprom();
-void read_eeprom(unsigned char favourite);
-void write_eeprom();
-void read_registers();
-void write_registers();
-void init_radio();
-void set_channel(unsigned char key);
+/*** BIT OPERATIONS ***/
+void sbit(volatile unsigned char *reg, unsigned char bit); // set bit in register
+void cbit(volatile unsigned char *reg, unsigned char bit); // clear bit in register
+void _sbit(unsigned char pin); // set bit on port C
+void dir_sda(unsigned char dir); // set SDA direction
+unsigned char read_sda(); // read bit on SDA
+
+/*** I2C (TWI) ***/
+void start(); // start bit
+void stp(); // stop bit
+void ack(); // acknowledgment bit
+void nack(); // no acknowledgment bit
+unsigned char read_byte(); // read 8-bits
+void write_byte(unsigned char data); // write 8-bits
+
+/*** ATmega328 ***/
+void init_interrupt(); // interrupt configuration
+void clr_eeprom(); // eeprom clean out from 0x08 up to 0x00 addresses
+void read_eeprom(unsigned char favourite); // read data from eeprom, address at current favourite
+void write_eeprom(); // write data into eeprom, address at current favourite
+
+/*** Si4703 ***/
+void read_registers(); // read all (16) registers from device
+void write_registers(); // device registers (6) update
+void init_radio(); // Si4703 initialization
+void set_channel(unsigned char key); // tune into selected channel / frequency
 void seek_channel(unsigned char key);
 void set_volume(unsigned char vol);
-void strobe();
-void write_display(unsigned char mode, unsigned char data);
-void init_display();
-void write_char(unsigned char data);
-void write_text(char *text);
-void display_info();
+
+/*** 2x16 LCD DISPLAY (HD44780) ***/
+void strobe(); // device strobe
+void write_display(unsigned char mode, unsigned char data); // write instruction or data to display
+void init_display(); // display initialization
+void write_char(unsigned char data); // display char
+void write_text(char *text); // display text
+void display_info(); // information display
 
 #ifdef __cplusplus
 }
